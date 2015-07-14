cbeefd69dab871e3d2814349c14b2574edbd191c
Serhii Aksiutin <serhiiaksiutin@gmail.com>
2015-07-14 16:09:12 -0700

    ignore 0 added

--- Problem 1.py
+++ Problem 1.py
@@ -3,7 +3,8 @@ i = []
 while len(i) < 30:
     i.append(random.randint(0, 100))
 print i
-
 for x in i:
-    if x % 2 == 0:
-        print x
+	if x is 0:
+		pass
+   	elif x % 2 == 0:
+        	print x
