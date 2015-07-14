4c855d6f0f41f7079767267915745eddedf46453
Serhii Aksiutin <serhiiaksiutin@gmail.com>
2015-07-14 16:11:19 -0700

    fixes

--- Problem 1.py
+++ Problem 1.py
@@ -4,7 +4,7 @@ while len(i) < 30:
     i.append(random.randint(0, 100))
 print i
 for x in i:
-	if x is 0:
-		pass
-   	elif x % 2 == 0:
-        	print x
+    if x is 0:
+        pass
+    elif x % 2 == 0:
+        print x
