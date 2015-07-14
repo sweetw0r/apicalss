229f0d02c81cfef6aac00f9e522ca7c207d8fe35
Serhii Aksiutin <serhiiaksiutin@gmail.com>
2015-07-14 15:55:53 -0700

    2 task - compute the number of even...

--- Problem 1.py
+++ Problem 1.py
@@ -1,5 +1,9 @@
-import random 
-i =[]
+import random
+i = []
 while len(i) < 30:
-	i.append(random.randint(0, 100))
+    i.append(random.randint(0, 100))
 print i
+
+for x in i:
+    if x % 2 == 0:
+        print x
