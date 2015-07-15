1f2ac54e704d78904cf00b2e47e61dc8c798b9fc
Serhii Aksiutin <serhiiaksiutin@gmail.com>
2015-07-14 17:39:53 -0700

    after debugging

--- Problem 1.py
+++ Problem 1.py
@@ -2,7 +2,7 @@ import random
 i = []
 while len(i) < 30:
     i.append(random.randint(0, 100))
-#print i #rows out of tasks should be commented
+# print i #rows out of tasks should be commented
 for x in i:
     if x is 0:
         pass
