800121ac41729cd7bc2131b1dbd105266f2ddf7a
Serhii Aksiutin <serhiiaksiutin@gmail.com>
2015-07-14 18:04:28 -0700

    Debugging ZeroDivisionError:

--- Problem 2.py
+++ Problem 2.py
@@ -4,21 +4,22 @@ while True:
         i = []
         while len(i) < 30:
             i.append(random.randint(0, 100))
-        # print i #rows out of tasks should be commented
-        # print i[0] #rows out of tasks should be commented
+        print i  #rows out of tasks should be commented
+        # print i[0]  #rows out of tasks should be commented
         max_val = i[0]
         min_val = i[0]
         for x in i:
-            #if x is 0:
-                #pass
             if x > max_val:
                 max_val = x
+        print 'Max is', max_val
         for x in i:
-            if x is 0:
+            if x == 0:
                 pass
             elif x < min_val:
-                min_val = x
+                if x != 0:
+                    min_val = x
+        print 'Min is', min_val
         print max_val % min_val
     except ZeroDivisionError:
         print "Stop"
-        break
+    
