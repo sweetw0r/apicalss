5bfdefe19740b1d243a30d472525ad94aff14bca
Serhii Aksiutin <serhiiaksiutin@gmail.com>
2015-07-22 17:29:33 -0700

    improves

--- hw_class_3_2.py
+++ hw_class_3_2.py
@@ -1,21 +1,15 @@
 from random import randint
 
+
 def sum_of(n, num_giv):
-    # num_giv = 30 # def sum_of(n, num_giv):
-    # n = 10 # def sum_of(n, num_giv): 
-    l = []  # generated list 
+    l = []  # generated list
     for v in range(n):
         l.append(randint(1, 20))
     print l
-    
-    L = len(l)  # L = 10
-    
-    
-    # found = 0
-    for i in range(L): # range(L)
-        for j in range(i + 1, L):
+
+    for i in range(n):  # range(n)
+        for j in range(i + 1, n):
             if l[i] + l[j] == num_giv:
-                
                 print l[i], 'plus', l[j], 'equal', num_giv
     return l
 print(sum_of(20, 30))
