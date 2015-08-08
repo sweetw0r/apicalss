18db43f524c9ac79616a2b8dccf88f76a9656556
Serhii Aksiutin <serhiiaksiutin@gmail.com>
2015-08-08 12:43:53 -0700

    update api_utils

--- api_framework/src/api_utils.py
+++ api_framework/src/api_utils.py
@@ -2,6 +2,7 @@ import requests
 import json
 from ConfigParser import SafeConfigParser
 import os
+import time
 
 
 # class Config:
@@ -27,8 +28,124 @@ class Config:
         self.domain = self.parser.get('Server', 'domain')
         self.admin_login = self.parser.get('Server', 'admin')
         self.password = self.parser.get('Server', 'password')
+        self.test_path = self.parser.get('Server', 'test_path')
 
-f = Config()
-print(f.admin_login)
-print(f.password)
+# f = Config()
+# print(f.admin_login)
+# print(f.password)
 
+
+class Response:
+    def __init__(self):
+        self.http_code = 0
+        self.jsom = dict()
+        self.headers = dict()
+
+
+class Calls:
+    def __init__(self):
+        self.config = Config()
+        self.no_json = 'noJson'
+
+    def create_folder(self, name, domain=None,
+                      username=None, password=None,
+                      content_type=None,
+                      accept=None, method=None, test_path=None):
+        if domain is None:
+            domain = self.config.domain
+        if username is None:
+            username = self.config.admin_login
+        if password is None:
+            password = self.config.password
+        if content_type is None:
+            content_type = 'application/json'
+        if accept is None:
+            accept = 'application/json'
+        if method is None:
+            method = 'POST'
+        if test_path is None:
+            test_path = self.config.test_path
+
+        endpoint = '/public-api/v1/fs/'
+        url = domain + endpoint + test_path + name
+        headers = dict()
+        headers['Content-Type'] = content_type
+        headers['Accept'] = accept
+        data = dict()
+        data['action'] = 'add_folder'
+        data = json.dumps(data)
+        r = requests.request(
+            url=url,
+            auth=(username, password),
+            headers=headers,
+            data=data,
+            method=method
+        )
+        try:
+            json_resp = json.loads(r.content)
+        except ValueError:
+            if method == 'OPTIONS':
+                json_resp = r.content
+            else:
+                json_resp = self.no_json
+
+        r.json = json_resp
+        response = Response()
+        response.http_code = r.status_code
+        response.json = r.json
+        response.headers = r.headers
+        return response
+
+    @staticmethod
+    def gen_random_name():
+        return 'dynamic_name_%s' % str(time.time()).replace('.', '')
+
+    def delete_folder(self, name, domain=None,
+                      username=None, password=None,
+                      content_type=None,
+                      accept=None, method=None, test_path=None):
+        if domain is None:
+            domain = self.config.domain
+        if username is None:
+            username = self.config.admin_login
+        if password is None:
+            password = self.config.password
+        if content_type is None:
+            content_type = 'application/json'
+        if accept is None:
+            accept = 'application/json'
+        if method is None:
+            method = 'DELETE'
+        if test_path is None:
+            test_path = self.config.test_path
+
+        endpoint = '/public-api/v1/fs/'
+        url = domain + endpoint + test_path + name
+        headers = dict()
+        headers['Content-Type'] = content_type
+        headers['Accept'] = accept
+        r = requests.request(
+            url=url,
+            auth=(username, password),
+            headers=headers,
+            method=method
+        )
+
+        try:
+            json_resp = json.loads(r.content)
+        except ValueError:
+            if method == 'OPTIONS':
+                json_resp = r.content
+            else:
+                json_resp = self.no_json
+
+        r.json = json_resp
+        response = Response()
+        response.http_code = r.status_code
+        response.json = r.json
+        response.headers = r.headers
+        return response
+
+        @staticmethod
+        def gen_random_name():
+            return 'dynamic_name_%s' % str(time.time()).replace('.', '')
