81e8833d27ec15659cd007ba3f53acc66de73908
Serhii Aksiutin <serhiiaksiutin@gmail.com>
2015-08-08 12:40:38 -0700

    test title added

---
+++ api_framework/tests/test_create_folder_api_script.py
@@ -0,0 +1,35 @@
+from api_utils import Calls
+from unittest import TestCase
+import httplib
+
+
+class TestClass(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.calls = Calls()
+
+    # @classmethod
+    # def tearDownClass(cls):
+    #     pass
+
+    # def SetUp(self):
+    #     pass
+
+    # def tearDown(cls):
+    #     pass
+
+    def test_create_folder_positive(self):
+        folder = self.calls.gen_random_name()
+        resp = self.calls.create_folder(folder)
+        assert resp.http_code == httplib.CREATED
+
+    def test_creat_folder_incorrect_creadentials(self):
+        folder = self.calls.gen_random_name()
+        resp = self.calls.create_folder(folder, password='asasaa')
+        assert resp.http_code == httplib.UNAUTHORIZED
+        print(resp.json)
+        assert resp.json['inputErrors']['credentials'][
+            0]['code'] == 'INVALID_CREDENTIALS'
+        assert resp.json['inputErrors']['credentials'][0]['msg'] == 'This request is unauthenticated. Please provide ' \
+                                                                    'credentials and try again.'
