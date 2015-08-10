f788de7ee97e0edfe1b1b15378296251d5d7e389
Serhii Aksiutin <serhiiaksiutin@gmail.com>
2015-08-10 01:38:39 -0700

    1st test for permissions is done

--- api_framework/tests/test_create_folder_api_script.py
+++ api_framework/tests/test_create_folder_api_script.py
@@ -9,16 +9,17 @@ class TestClass(TestCase):
     @classmethod
     def setUpClass(cls):
         cls.calls = Calls()
+        cls.config = Config()
 
-    # @classmethod
-    # def tearDownClass(cls):
-    #     pass
+    @classmethod
+    def tearDownClass(cls):
+        pass
 
-    # def SetUp(self):
-    #     pass
+    def SetUp(self):
+        pass
 
-    # def tearDown(cls):
-    #     pass
+    def tearDown(cls):
+        pass
 
     def test_create_folder_positive(self):
         folder = self.calls.gen_random_name()
@@ -32,14 +33,14 @@ class TestClass(TestCase):
         print(resp.json)
         assert resp.json['inputErrors']['credentials'][
             0]['code'] == 'INVALID_CREDENTIALS'
-        assert resp.json['inputErrors']['credentials'][0]['msg'] == 'This request is unauthenticated. Please provide ' \
-                                                                    'credentials and try again.'
-    
-    # def test_perm(self):
-    #     folder = self.calls.gen_random_name()
-    #     resp = self.calls.create_folder(folder)
-    #     assert resp.http_code == httplib.OK
-    #     resp = self.calls.test_user_permision(user=self.config.puser, test_path='%s') % (folder)
-    #     assert resp.http_code == httplib.CREATED
+        assert resp.json['inputErrors']['credentials'][
+            0]['msg'] == 'This request is unauthenticated.'\
+            ' Please provide credentials and try again.'
 
-
+    def test_perm(self):
+        folder = self.calls.gen_random_name()
+        resp = self.calls.create_folder(folder)
+        assert resp.http_code == httplib.CREATED
+        resp = self.calls.user_permision(
+            folder, endpoint='/public-api/v1/perms/folder/')
+        assert resp.http_code == httplib.OK
